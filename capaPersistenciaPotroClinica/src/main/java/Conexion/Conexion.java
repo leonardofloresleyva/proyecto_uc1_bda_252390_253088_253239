1e35c94501c61f215d4f84ae31b53120fc3aff01
Leonardo Flores Leyva <87454042+leonardofloresleyva@users.noreply.github.com>
2025-02-19 00:51:30 -0700

    Me faltó agregar el resto de paquetes aparentemente. En este momento estaba aprendiendo a usar Git con NetBeans, una disculpa.

---
+++ capaPersistenciaPotroClinica/src/main/java/Conexion/Conexion.java
@@ -0,0 +1,11 @@
+package Conexion;
+
+/**
+ *
+ * @author Leonardo Flores Leyva (252390)
+ * @author Ximena Rosales Panduro (253088)
+ * @author Luis Uribe (253239)
+ */
+public class Conexion {
+    
+}
