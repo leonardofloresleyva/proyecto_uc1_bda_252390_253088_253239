0e61242e58c5df400c8ac004e734a8e2d97a4377
LuisUribe14 <luis.uribe253239@potros.itson.edu.mx>
2025-02-19 17:41:03 -0700

    CapaNegocio

---
+++ capaNegocioPotroClinica/src/main/java/com/mycompany/capanegociopotroclinica/CapaNegocioPotroClinica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.capanegociopotroclinica;
+
+/**
+ *
+ * @author multaslokas33
+ */
+public class CapaNegocioPotroClinica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
