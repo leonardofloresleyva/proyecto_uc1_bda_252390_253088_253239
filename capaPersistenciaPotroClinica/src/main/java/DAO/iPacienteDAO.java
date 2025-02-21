cf03ad2229cb12eb9c98e1e78c5d086c8a16eb55
Leonardo Flores Leyva <87454042+leonardofloresleyva@users.noreply.github.com>
2025-02-21 00:27:20 -0700

    Correciones de merge con Master

---
+++ capaPersistenciaPotroClinica/src/main/java/DAO/iPacienteDAO.java
@@ -0,0 +1,22 @@
+package DAO;
+
+import Entidades.Paciente;
+import Excepciones.PersistenciaException;
+
+/**
+ * Interfaz del paciente
+ * @author Leonardo Flores Leyva (252390)
+ * @author Ximena Rosales Panduro (253088)
+ * @author Luis Uribe (253239)
+ */
+public interface iPacienteDAO {
+    
+    /**
+     * Método que registra un nuevo paciente
+     * @param paciente Objeto de tipo paciente
+     * @return True si se registró el paciente, false en caso contrario
+     * @throws PersistenciaException 
+     */
+    public boolean registrarPaciente(Paciente paciente) throws PersistenciaException;
+    
+}
