7dd4f8cf1232f452de25900ea07f1117f358f11b
Leonardo Flores Leyva <87454042+leonardofloresleyva@users.noreply.github.com>
2025-02-20 18:19:19 -0700

    Recuperación de cambios.
    En el anterior commit quité sin querer los cambios hechos a las clases PacienteBO y PacienteMapper. En este commit, se suben las clases nuevas ahora sí.

--- capaNegocioPotroClinica/src/main/java/BO/PacienteBO.java
+++ capaNegocioPotroClinica/src/main/java/BO/PacienteBO.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package BO;
 
 import Conexion.iConexion;
@@ -19,10 +15,12 @@ import java.util.regex.Pattern;
 
 /**
  *
- * @author multaslokas33
+ * @author Leonardo Flores Leyva (252390)
+ * @author Ximena Rosales Panduro (253088)
+ * @author Luis Eduardo Uribe Vega (253239)
  */
 public class PacienteBO {
-    iPacienteDAO pacienteDAO;
+    private final iPacienteDAO pacienteDAO;
     
     public PacienteBO (iConexion conexion) {
         this.pacienteDAO = new PacienteDAO(conexion);
@@ -30,41 +28,81 @@ public class PacienteBO {
     
      public boolean registrarPaciente(PacienteNuevoDTO paciNuevo) throws NegocioException {
         
+        String correo = paciNuevo.getUsuario();
+        String password = paciNuevo.getContrasenia();
+        String rol = paciNuevo.getRol();
         String nombre = paciNuevo.getNombres();
+        String apellidoP = paciNuevo.getApellidoPaterno();
+        String apellidoM = paciNuevo.getApellidoMaterno();
         String telefono = paciNuevo.getTelefono();
-        String apellidoPaterno = paciNuevo.getApellidoPaterno();
+        LocalDate fechaN = paciNuevo.getFechaNacimiento();
         
-        if (paciNuevo == null) {
+        if (paciNuevo == null) 
             throw new NegocioException("El paciente no puede ser nulo.");
-        }
-        if (nombre == null || nombre.trim().isEmpty()) {
-            throw new NegocioException("El nombre no puede estar vacío.");
-        }
-        if (apellidoPaterno == null || apellidoPaterno.trim().isEmpty()) {
+        
+        if(correo == null || correo.trim().isEmpty())
+            throw new NegocioException("El correo no puede estar vacio.");
+        
+        if(correo.length() > 150)
+            throw new NegocioException("No se permiten correos con mas de 150 caracteres.");
+        
+        if (!Pattern.matches("^[^@\\s]+@[^@\\s]+\\.com$", correo)) 
+             throw new NegocioException("La cadena ingresada no es un correo electronico.");
+        
+        if (password == null || password.trim().isEmpty()) 
+            throw new NegocioException("La contrasenia no puede estar vacia.");
+        
+        if(password.length() > 20)
+            throw new NegocioException("No se permiten contrasenias con mas de 20 caracteres.");
+        
+        if(rol == null || rol.trim().isEmpty())
+            throw new NegocioException("El rol no puede estar vacío.");
+        
+        if(!"Paciente".equals(rol))
+            throw new NegocioException("El rol debe ser igual a \"Paciente\".");
+        
+        if (nombre == null || nombre.trim().isEmpty()) 
+            throw new NegocioException("El nombre no puede estar vacio.");
+        
+        if (!Pattern.matches("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", nombre)) 
+            throw new NegocioException("No se admiten caracteres especiales en el nombre.");
+        
+        if(nombre.length() > 100)
+            throw new NegocioException("No se permiten nombres con mas de 100 caracteres.");
+        
+        if (apellidoP == null || apellidoP.trim().isEmpty()) 
             throw new NegocioException("El apellido paterno no puede estar vacío.");
-        }
-        if (telefono == null || telefono.trim().isEmpty()) {
+        
+        if (!Pattern.matches("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", apellidoP)) 
+            throw new NegocioException("No se admiten caracteres especiales en el apellido paterno");
+        
+        if(apellidoP.length() > 100)
+            throw new NegocioException("No se permiten apellidos paternos con mas de 100 caracteres.");
+        
+        if (apellidoM == null || apellidoM.trim().isEmpty()) 
+            throw new NegocioException("El apellido materno no puede estar vacío.");
+        
+        if (!Pattern.matches("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", apellidoM)) 
+            throw new NegocioException("No se admiten caracteres especiales en el apellido materno");
+        
+        if(apellidoM.length() > 100)
+            throw new NegocioException("No se permiten apellidos maternos con mas de 100 caracteres.");
+        
+        if (telefono == null || telefono.trim().isEmpty()) 
             throw new NegocioException("El teléfono no puede estar vacío.");
-        }
-        if (telefono.length()>20){
-        throw new NegocioException("No se pueden más de 20 caracteres en el telefono");
-        }
-        if (paciNuevo.getFechaNacimiento() == null) {
-            throw new NegocioException("La fecha de nacimiento es obligatoria.");
-        }
-         if (paciNuevo.getFechaNacimiento().isAfter(LocalDate.now())) {
-            throw new NegocioException("La fecha de nacimiento no puede ser futura");
-        }
-        if (!Pattern.matches("^[a-zA-ZáéíóúÁÉÍÓÚñÑ ]+$", nombre)) {
-            throw new NegocioException("No se admiten caracteres especiales en el nombre");
-        }
-        /**
-        if (paciNuevo.getContrasenia().length() > 20) {
-            throw new NegocioException("No se pueden más de 20 caracteres en la contraseña");
-        }*/
+        
+        if (telefono.length() > 20)
+            throw new NegocioException("No se permiten telefonos con mas de 20 caracteres.");
+        
+        if (fechaN == null) 
+            throw new NegocioException("La fecha de nacimiento no puede estar vacia.");
+        
+        if (fechaN.isAfter(LocalDate.now())) 
+            throw new NegocioException("La fecha de nacimiento no puede estar después de la fecha actual.");
+        
         
         PacienteMapper convertidor = new PacienteMapper();
-        Paciente paciente = convertidor.ToEntity(paciNuevo);
+        Paciente paciente = convertidor.toEntityNuevo(paciNuevo);
 
         try {
             pacienteDAO.registrarPaciente(paciente);
