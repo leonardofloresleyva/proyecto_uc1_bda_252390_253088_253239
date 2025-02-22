0c59738f6a09c984e473c8d87cd5dc2e2dc0da97
LuisUribe14 <luis.uribe253239@potros.itson.edu.mx>
2025-02-22 01:15:23 -0700

    capaPresentacion

---
+++ capaPresentacionPotroClinica/src/main/java/com/mycompany/capapresentacionpotroclinica/CapaPresentacionPotroClinica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.capapresentacionpotroclinica;
+
+/**
+ *
+ * @author multaslokas33
+ */
+public class CapaPresentacionPotroClinica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
